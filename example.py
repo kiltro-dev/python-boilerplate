3b07bb53d97966ed7fb777e71d9e2f37937b1e84
Ignacio H <55958716+kiltro-dev@users.noreply.github.com>
2023-10-29 19:16:54 -0300

    config: add exceptions for pylint

--- example.py
+++ example.py
@@ -1,9 +1,6 @@
-'''Module description'''
-
 def this_is_my_function():
-    '''Function description'''
     print('Hello')
 
+
 def my_function():
-    '''Function description'''
     print('Hello')
